9752bc6a956f4bf4ea85b337ff56602091bddc2c
Red Carpet <redcarpetoaxaca@gmail.com>
2019-07-29 17:38:25 +0000

    print block

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() {
        index: 0,
        time_stamp: String::from("timestamp"),
        data: 0,
-       hash: String.from("hash"),
-       previous_hash: String.from("previous hash")
-   }
+       hash: String::from("hash"),
+       previous_hash: String::from("previous hash")
+   };
 
-   println!("{}",b);
+   println!("{}{} {} {} {}",b.index, b.time_stamp, b.data, b.hash, b.previous_hash);
 }
