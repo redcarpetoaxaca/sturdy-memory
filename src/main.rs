b5051cdb1eacc383c88fa199ad01203dd015a6da
Red Carpet <redcarpetoaxaca@gmail.com>
2019-07-29 17:29:08 +0000

    print block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+
+   struck Block {
+     index: u64,
+     time_stamp: String,
+     data: u128,
+     hash: String,
+     previous_hash: String
+   }
+   
+   let b = Block {
+       index: 0,
+       time_stamp: String::from("timestamp"),
+       data: 0,
+       hash: String.from("hash"),
+       previous_hash: String.from("previous hash")
+   }
+
+   println!("{}",b);
 }
