50cbb24aa0ba225bdffa9eb59505a59025e2aa0d
Red Carpet <redcarpetoaxaca@gmail.com>
2019-07-30 21:55:04 +0000

    add pest

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
-extern crate chrono;
-use chrono::{DateTime, TimeZone, NaiveDateTime,Utc};
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "csv.pest"]
+pub struct CSVParser;
 
   fn main() {
-      let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(61, 0), Utc);
-      assert_eq!(Utc.timestamp(61, 0), dt);
-
-
-   struct Block {
-     index: u64,
-     time_stamp: String,
-     data: u128,
-     hash: String,
-     previous_hash: String
-   };
-   
-   fn build_block(index: u64, 
-                  time_stamp: String, 
-                  data: u128, 
-                  hash: String, 
-                  previous_hash: String) -> Block  {
-    Block {
-     index,
-     time_stamp,
-     data,
-     hash,
-     previous_hash
-   }
-                  }
-
-   let b = build_block(0,
-                       String::from("now"),
-                       0, 
-                       String::from("fadaf"), 
-                       String::from("previous"));
-
-   
-   println!("{}{} {} {} {}",b.index, b.time_stamp, b.data, b.hash, b.previous_hash);
 }
